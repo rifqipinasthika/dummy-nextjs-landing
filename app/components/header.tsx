6ba9b7f1d0fcd928d9f5d4be25401d0914527a54
Rifqi Pinasthika <rifqi.pinasthika@kalyana.co.id>
2023-07-12 14:14:17 +0700

    initial setup link

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,17 +1,18 @@
 import React from 'react'
+import Link from 'next/link'
 
 export const Header = () => {
     return (
         <header className="text-gray-600 body-font">
             <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-                <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+                <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0" href='/'>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-10 h-10 text-white p-2 bg-yellow-500 rounded-full" viewBox="0 0 24 24">
                         <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
                     </svg>
-                    <span className="ml-3 text-xl">Jamesblocks</span>
+                    <span className="ml-3 text-xl">Goblocks</span>
                 </a>
                 <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-                    <a className="mr-5 hover:text-gray-900">First Link</a>
+                    <a className="mr-5 hover:text-gray-900" href='/product'>Product</a>
                     <a className="mr-5 hover:text-gray-900">Second Link</a>
                     <a className="mr-5 hover:text-gray-900">Third Link</a>
                     <a className="mr-5 hover:text-gray-900">Fourth Link</a>
---
+++ app/product/page.tsx
@@ -0,0 +1,7 @@
+export default function Product() {
+    return (
+        <main>
+            <h1>This is product page</h1>
+        </main>
+    )
+}
