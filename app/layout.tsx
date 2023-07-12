547bdb4175b94c27080a69d54d20b7277d13ec31
Rifqi Pinasthika <rifqi.pinasthika@kalyana.co.id>
2023-07-12 15:15:10 +0700

    update link

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from 'next/font/google'
 import { Header } from './components/header'
 import { Footer } from './components/footer'
 
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -19,10 +20,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header/>
+        <Header />
         {children}
-        <Footer/>
-        </body>
+        <Footer />
+      </body>
     </html>
   )
 }
